2d17ef0580c94c0e8c59fbb74d68fe062b92b7ad
Kenji Yamada <yamagmusu@gmail.com>
2024-11-16 00:59:50 +0900

    BGM

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,37 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID BGM_NORMALBATTLE = 3652025539U;
+        static const AkUniqueID BGM_BATTLESETTINGS = 2316661597U;
+        static const AkUniqueID BGM_LOSS = 1142577437U;
+        static const AkUniqueID BGM_NORMALBATTLE01 = 2971848282U;
+        static const AkUniqueID BGM_NORMALBATTLE02 = 2971848281U;
+        static const AkUniqueID BGM_NORMALBATTLE03 = 2971848280U;
+        static const AkUniqueID BGM_NORMALBATTLE04 = 2971848287U;
+        static const AkUniqueID BGM_RECOVERROOM = 801326845U;
+        static const AkUniqueID BGM_TOWN = 3507265084U;
+        static const AkUniqueID BGM_VICTORY = 4222241002U;
+        static const AkUniqueID SE_APPEARANCE = 25405498U;
+        static const AkUniqueID SE_AXE = 3492321120U;
+        static const AkUniqueID SE_BOW = 3643569840U;
+        static const AkUniqueID SE_CRETICAL = 2626094531U;
+        static const AkUniqueID SE_CURE = 4165363523U;
+        static const AkUniqueID SE_HEAL = 742698618U;
+        static const AkUniqueID SE_HIT = 3141227107U;
+        static const AkUniqueID SE_MAGIC = 1604393909U;
+        static const AkUniqueID SE_SHIELD = 3951230663U;
+        static const AkUniqueID SE_SLASH = 2885307791U;
+        static const AkUniqueID ST_BGCHANGE = 592837134U;
+        static const AkUniqueID ST_BUY = 3520170161U;
+        static const AkUniqueID ST_CAMZOOM = 1851990029U;
+        static const AkUniqueID ST_CHANGEEQP = 1429415107U;
+        static const AkUniqueID ST_CLICK = 2645564349U;
+        static const AkUniqueID ST_ERROR = 1828652115U;
+        static const AkUniqueID ST_FIT = 3049808428U;
+        static const AkUniqueID ST_GRIP = 4147925143U;
+        static const AkUniqueID ST_LVUPEQP = 3623674276U;
+        static const AkUniqueID ST_LVUPUNIT = 3358621300U;
+        static const AkUniqueID ST_RECOVER = 188759585U;
+        static const AkUniqueID ST_TRASH = 126945167U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
@@ -33,7 +63,9 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+        static const AkUniqueID BGMSOUNDBANK = 3370353384U;
+        static const AkUniqueID SESOUNDBANK = 808718364U;
+        static const AkUniqueID SYSTEMSOUNDBANK = 1625944053U;
     } // namespace BANKS
 
     namespace BUSSES
