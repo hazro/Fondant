a21686739e6d69b996edcc4f3e3cee8bf1e958cc
Kenji Yamada <yamagmusu@gmail.com>
2024-11-19 11:20:31 +0900

    不要ファイル削除

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -49,6 +49,8 @@ namespace AK
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID BGMVOLUME = 3043206523U;
+        static const AkUniqueID SEVOLUME = 3362400191U;
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
         static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
         static const AkUniqueID SS_AIR_FURY = 1029930033U;
@@ -59,6 +61,7 @@ namespace AK
         static const AkUniqueID SS_AIR_STORM = 3715662592U;
         static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
         static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID SYSTEMVOLUME = 2417814656U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
